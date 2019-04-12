8739668928cf139c9a21f702a43e858d0d82916a
Eric Raio <eric@ericraio.com>
2019-04-12 11:55:00 -0700

    test

--- lib/turbolinks_render/middleware.rb
+++ lib/turbolinks_render/middleware.rb
@@ -39,7 +39,7 @@ module TurbolinksRender
       end
 
       def js_code_to_render_html(html)
-        encoded_html = html.encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '').force_encoding("UTF-8")
+        encoded_html = html.force_encoding(Encoding::UTF_8)
         escaped_html = ActionController::Base.helpers.j(encoded_html)
         <<-JS
         (function(){
